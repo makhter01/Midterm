636bc73e4b089203d7fa9f9ba57a5dcc79014b43
makhter01 <mehzabinakhter@gmail.com>
2017-12-09 15:34:40 -0500

    first commit

--- src/quiz/SampleQuiz.java
+++ src/quiz/SampleQuiz.java
@@ -5,5 +5,8 @@ public class SampleQuiz {
     /**
      * Create a program that prints out "Hello World!"
      */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 
 }
